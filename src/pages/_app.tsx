6ccf18ee9976b845c359c469295ca245a2328459
Jorge Zerpa <jorgezerpacoder@gmail.com>
2023-03-08 13:04:39 -0400

    add loading logic on first render

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,26 @@
+import { useEffect, useState } from 'react'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import MainLayout from '@/layouts/MainLayout'
 
+
 export default function App({ Component, pageProps }: AppProps) {
+  const [isLoading, setIsLoading] = useState(false)
+  
+  useEffect(()=>{
+    setIsLoading(false)
+  }, [])
+
   return (
     <>
-      <MainLayout>
-        <Component {...pageProps} />
-      </MainLayout>
+      { isLoading 
+        ? <div>lOADING...</div> 
+        : (
+          <MainLayout>
+            <Component {...pageProps} />
+          </MainLayout>
+        )
+      }
     </>
   )
 }
