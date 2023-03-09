7077a2d6290e64b8b5f97786f92fd32e0150beee
Jorge Zerpa <jorgezerpacoder@gmail.com>
2023-03-09 07:06:41 -0400

    incorporate phone number input component

--- src/components/form/TextInput.tsx
+++ src/components/form/TextInput.tsx
@@ -4,7 +4,7 @@ import { ValidIndicator } from './ValidIndicator'
 export const TextInput = ({ value, changeHandler, isValid, placeHolder="", isRequired=false }:{ value:string, changeHandler:(value:string)=>void, isValid:boolean, placeHolder?:string, isRequired?:boolean }) => {
   return (
     <div className='flex-1 flex flex-col mb-12 relative'>
-        <input value={value} onChange={(e)=>changeHandler(e.target.value)} type="text" className="rounded-lg bg-transparent border outline-none border-gray-500 focus:border-white text-gray-200 block w-full p-2 text-lg" placeholder={placeHolder} required={isRequired}></input>
+        <input value={value} onChange={(e)=>changeHandler(e.target.value)} type="text" className="rounded-lg bg-transparent border outline-none border-gray-500 focus:border-white text-white placeholder:text-gray-300 block w-full p-2 text-lg" placeholder={placeHolder} required={isRequired}></input>
         <ValidIndicator isValid={isValid} />
     </div>
   )
