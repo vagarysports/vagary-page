2011f45a4f7dfba41a6925a420c0a5ebd9e18d95
Jorge Zerpa <jorgezerpacoder@gmail.com>
2023-03-12 06:31:55 -0400

    improve navbar responsiveness

--- src/commons/Navbar.tsx
+++ src/commons/Navbar.tsx
@@ -24,18 +24,18 @@ export const Navbar = () => {
 
   return (
     <nav className="fixed z-40 top-0 left-0 w-full border-gray-200 px-2 py-2.5 bg-black ">
-        <div className="flex items-center justify-between md:justify-start w-full">
+        <div className="flex items-center justify-between lg:justify-start w-full">
 
             <Link href={'/'} className="block text-white rounded">
                 <Image src={'/logos/logo.png'} height={58} width={142} alt="Vagary" />
             </Link>
 
-            <button onClick={()=>setShowMenu(!showMenu)} type="button" className="inline-flex items-center ml-3 text-sm text-gray-500 rounded-lg md:hidden" aria-controls="navbar-default" aria-expanded="false">
+            <button onClick={()=>setShowMenu(!showMenu)} type="button" className="inline-flex items-center ml-3 text-sm text-gray-500 rounded-lg lg:hidden" aria-controls="navbar-default" aria-expanded="false">
                 <span className="sr-only">Open main menu</span>
                 <MdMenu size={40} color="white" />
             </button>
 
-            <div className={`${showMenu?'-translate-x-0':'-translate-x-[240px]'} transition-all md:hidden z-50 fixed top-0 bottom-0 left-0 w-[240px] bg-black text-white p-2`}>
+            <div className={`${showMenu?'-translate-x-0':'-translate-x-[240px]'} transition-all lg:hidden z-50 fixed top-0 bottom-0 left-0 w-[240px] bg-black text-white p-2`}>
                 <div onClick={()=>setShowMenu(!showMenu)} className="w-full flex justify-center border-b border-b-white pb-2 mt-2">
                     <Link href={'/'} className="block text-white rounded">
                         <Image src={'/logos/logo.png'} height={58} width={142} alt="Vagary" />
@@ -56,11 +56,11 @@ export const Navbar = () => {
                 </ul>
             </div>
 
-            <div className="hidden w-full md:block md:w-auto ml-10 " id="navbar-default">
+            <div className="hidden w-full lg:block lg:w-auto ml-10 " id="navbar-default">
                 <ul className="flex flex-col p-4 mt-4  md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
                     {ITEMS.map(item=>(
                         <li key={"desktopmenu"+item.path}>
-                            <Link href={item.path} className="text-xl block text-white rounded relative">
+                            <Link href={item.path} className="text-xl  block text-white rounded relative">
                                 <span className="inline-block relative">
                                     { item.name }
                                     <div className={`${pathname===item.path?'scale-x-1':'scale-x-0'} absolute top-[120%] origin-left transition-all w-full h-[1px] bg-white`}></div>
