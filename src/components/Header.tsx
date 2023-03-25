fa0398e16538209e01657ef9aa3838e5f88eb8c5
Jorge Zerpa <jorgezerpacoder@gmail.com>
2023-03-25 15:43:25 -0400

    add scroll button to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
+import { ScrollDownButton } from './ScrollDownButton'
 
 type Props = {
     title: string
@@ -11,29 +12,35 @@ export const Header = ({ title, description, bgImage }:Props) => {
   return (
     <>
         { !description && (
-            <div className='relative w-full bg-center bg-cover bg-no-repeat bg-fixed' style={{ backgroundImage:`url(${bgImage})` }}>
-                <div className='rotate-0 bg-black py-20 md:py-56 bg-opacity-70 top-0 left-0 right-0 bottom-0 flex flex-col md:flex-row justify-center items-center  gap-5'>
-                <div className='w-[150px] md:w-[200px] relative min-h-[300px] opacity-0 animate-[fadeIn_.5s_ease_forwards_.5s]'>
-                    <Image alt="" src="/logos/logo-vert.png" fill style={{ objectFit:'contain' }}/>
-                </div>
-                <div className='w-[1px] h-[300px] bg-white hidden md:block animate-[scaleY_.5s_ease_forwards] origin-center'></div>
-                <div className='animate-[scaleY_.5s_ease_forwards_.5s] opacity-0 origin-top'>
-                    <h2 className='text-6xl sm:text-8xl font-bold text-white'>{ title }</h2>
-                </div>
+            <div className=' relative w-full bg-center bg-cover bg-no-repeat bg-fixed' style={{ backgroundImage:`url(${bgImage})` }}>
+                <div className='rotate-0 bg-black py-20 md:py-56 bg-opacity-70 top-0 left-0 right-0 bottom-0'>
+                    <div className='flex flex-col md:flex-row justify-center items-center  gap-5'>
+                        <div className='w-[150px] md:w-[200px] relative min-h-[250px] md:min-h-[300px] opacity-0 animate-[fadeIn_.5s_ease_forwards_.5s]'>
+                            <Image alt="" src="/logos/logo-vert.png" fill style={{ objectFit:'contain' }}/>
+                        </div>
+                        <div className='w-[1px] h-[300px] bg-white hidden md:block animate-[scaleY_.5s_ease_forwards] origin-center'></div>
+                        <div className='animate-[scaleY_.5s_ease_forwards_.5s] opacity-0 origin-top'>
+                            <h2 className='text-6xl sm:text-8xl font-bold text-white text-center md:text-left'>{ title }</h2>
+                        </div>
+                    </div>
+                    <ScrollDownButton />
                 </div>
             </div>
         )}
         { description && (
             <div className='relative w-full bg-center bg-cover bg-no-repeat bg-fixed' style={{ backgroundImage:'url(/images/partners-card.png)' }}>
-                <div className='rotate-0 bg-black py-20 md:py-56 bg-opacity-70 top-0 left-0 right-0 bottom-0 flex flex-col md:flex-row justify-center items-center  gap-5'>
-                    <div className='w-[150px] md:w-[200px] relative min-h-[300px] opacity-0 animate-[fadeIn_.5s_ease_forwards_.5s]'>
-                    <Image alt="" src="/logos/logo-vert.png" fill style={{ objectFit:'contain' }}/>
+                <div className='rotate-0 bg-black py-20 md:py-56 bg-opacity-70 top-0 left-0 right-0 bottom-0 flex flex-col justify-center items-center  gap-5'>
+                    <div className='flex flex-col md:flex-row justify-center items-center  gap-5'>
+                        <div className='w-[150px] md:w-[200px] relative min-h-[300px] opacity-0 animate-[fadeIn_.5s_ease_forwards_.5s]'>
+                            <Image alt="" src="/logos/logo-vert.png" fill style={{ objectFit:'contain' }}/>
+                        </div>
+                        <div className='w-[1px] h-[300px] bg-white hidden md:block animate-[scaleY_.5s_ease_forwards] origin-center'></div>
+                        <div className='animate-[scaleY_.5s_ease_forwards_.5s] opacity-0 origin-top'>
+                            <h2 className='text-white text-3xl font-bold text-center md:text-left mb-3'>{ title }</h2>
+                            <p className='tracking-wider max-w-[500px] text-white text-justify px-3 md:text-lg '>{ description }</p>
+                        </div>
                     </div>
-                    <div className='w-[1px] h-[300px] bg-white hidden md:block animate-[scaleY_.5s_ease_forwards] origin-center'></div>
-                    <div className='animate-[scaleY_.5s_ease_forwards_.5s] opacity-0 origin-top'>
-                    <h2 className='text-white text-3xl font-bold text-center md:text-left mb-3'>{ title }</h2>
-                    <p className='tracking-wider max-w-[500px] text-white text-justify px-3 md:text-lg '>{ description }</p>
-                    </div>
+                    <ScrollDownButton />
                 </div>
             </div>
         )}
--- src/components/ScrollDownButton.tsx
+++ src/components/ScrollDownButton.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
+import styles from '../styles/ScrollDownButton.module.css'
 
 export const ScrollDownButton = ({  }) => {
     const [fixedPosition, setFixedPosition] = useState(false)
@@ -15,9 +16,11 @@ export const ScrollDownButton = ({  }) => {
         }}
         className={`text-yellow-500 z-50 transition-all flex justify-center items-center gap-0 flex-col`}
     >
-        <svg className={'hover:animate-none animate-[scrollDownButton_2s_ease-in-out_infinite_0s]'} width="30" viewBox="0 0 32 17" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 1L15.2929 15.2929C15.6834 15.6834 16.3166 15.6834 16.7071 15.2929L31 1" stroke="#FFBC00" stroke-width="2"/></svg>
-        <svg className={'hover:animate-none animate-[scrollDownButton_2s_ease-in-out_infinite_0.2s]'} width="35" viewBox="0 0 32 17" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 1L15.2929 15.2929C15.6834 15.6834 16.3166 15.6834 16.7071 15.2929L31 1" stroke="#FFBC00" stroke-width="2"/></svg>
-        <svg className={'hover:animate-none animate-[scrollDownButton_2s_ease-in-out_infinite_0.4s]'} width="40" viewBox="0 0 32 17" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 1L15.2929 15.2929C15.6834 15.6834 16.3166 15.6834 16.7071 15.2929L31 1" stroke="#FFBC00" stroke-width="2"/></svg>
+        <svg className={styles['arrows']}>
+            <path className={styles['a1']} d="M0 0 L30 32 L60 0"></path>
+            <path className={styles['a2']} d="M0 20 L30 52 L60 20"></path>
+            <path className={styles['a3']} d="M0 40 L30 72 L60 40"></path>
+        </svg>
     </div>
   )
 }
