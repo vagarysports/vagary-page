ee15df0f8af124608d3e2d50d4cee1f81f377e53
Jorge Zerpa <jorgezerpacoder@gmail.com>
2023-03-25 17:52:29 -0400

    adapt UI to new requirements part2 (modifs on partners and sponsors)

--- src/pages/partnersAndSponsors/index.tsx
+++ src/pages/partnersAndSponsors/index.tsx
@@ -10,17 +10,13 @@ const PartnersAndSponsors = () => {
 
 
       <div id='partnersSlide1' className='bg-[#121618] bg-gradient-to-b from-[#121212] to-[#141414] px-5 md:px-20 py-5 md:py-32'>
-        <p className='text-white text-lg md:text-2xl tracking-wider  text-center'>In addition, the Ebora Formacion Sports City is the headquarters of the Campus Experience and the Social Sports Schools of the Real Madrid Foundation. The company is also in the process of building a new multipurpose building, a space with stands to accommodate the public attending competitions, and a new leisure space to complement the current cafeteria service.</p>
+        <p className='text-white text-lg md:text-2xl tracking-wider text-justify  sm:text-center'>In addition, the Ebora Formacion Sports City is the headquarters of the Campus Experience and the Social Sports Schools of the Real Madrid Foundation. The company is also in the process of building a new multipurpose building, a space with stands to accommodate the public attending competitions, and a new leisure space to complement the current cafeteria service.</p>
       </div>
 
       <div className=' bg-center bg-fixed bg-no-repeat bg-cover' style={{ backgroundImage:'url(/images/footer-1.png)' }}>
           <div className='bg-black bg-opacity-70 py-20 lg:py-56  w-full h-full flex flex-col md:flex-row justify-center items-center'>
-            <div className='flex-1 '>
-              <div className='w-full relative min-h-[300px] min-w-[300px] md:min-h-[300px]'>
-                <Image alt="" src="/images/real-levante.png" fill style={{ objectFit:'contain' }}/>
-              </div>
-            </div>
-            <div className='flex-1 p-5 md:p-10 md:px-10 '>
+            
+            <div className='flex-1 p-5 md:p-10 md:px-10 max-w-[700px] '>
               <p className='text-[#dedede] text-lg md:text-2xl  text-justify tracking-wider'>The partnership between Levante UD and Ebora Formacion, which was signed in 2021, aims to facilitate the transfer of the training and methodological identity of the Levante UD brand to Castilla - La Mancha, and specifically to the town of Cazalegas where the Sports City is located. Both entities will work together to promote the development of young footballers and coaches through opportunities, experiences, and training within Levante UD.</p>
             </div>        
           </div>
@@ -38,7 +34,7 @@ const PartnersAndSponsors = () => {
               <h2 className='text-center md:text-justify text-2xl text-gray-900 font-bold mb-10'>Coaching Training and <br /> Development</h2>
               <p className='text-lg md:text-xl  font-medium text-justify text-gray-900'>Our partnership with construyendo futbol allows our clients to have access to private coaching courses, physical preparation and analysis of tactical formations by coaches working in the best clubs in Spain such as Real madrid, Levante, etc… </p>
               <div className='w-full flex justify-center mt-20 px-3 md:px-10 '>
-                <button className='border border-gray-700 w-full py-2 text-xl text-gray-700 rounded-2xl'>See page</button>
+                <button className='border bg-gradient-to-b from-[#000] to-[#222]  rounded-md w-full py-2 text-xl text-white font-bold '>See page</button>
               </div>
             </div>        
         </div>
@@ -47,20 +43,16 @@ const PartnersAndSponsors = () => {
 
       <div className=' bg-center  bg-fixed bg-no-repeat bg-cover' style={{ backgroundImage:'url(/images/footer-2.png)' }}>
           <div className='py-20 lg:py-56 w-full h-full bg-black bg-opacity-70 flex flex-col md:flex-row-reverse justify-center items-center'>
-            <div className='flex-1 '>
-              <div className='w-full relative min-h-[300px] min-w-[300px] md:min-h-[300px]'>
-                <Image alt="" src="/images/cazalegas.png" fill style={{ objectFit:'contain' }}/>
-              </div>
-            </div>
-            <div className='flex-1 p-5 md:p-10 md:px-10 '>
+            
+            <div className='max-w-[700px] flex-1 p-5 md:p-10 md:px-10 '>
               <p className='text-[#dedede] text-lg md:text-2xl  text-justify'>In addition to its partnerships with major soccer organizations, Vagary Sports Management has also formed an alliance with the Club Deportivo Cazalegas and Ebora Formacion. This partnership, announced in 2020, is committed to building a professional and enthusiastic project that aims to be different from the rest. All parties involved in the partnership emphasized the importance of taking this project seriously and working towards goals. </p>
             </div>        
           </div>
       </div>
 
-      <div className='py-20'>
+      {/* <div className='py-20'>
         <CTA1 />
-      </div>
+      </div> */}
 
     </>
   )
